be91bf25e5fca3e0f75139377a4d35ede6e591e9
Franco Liptak <francoliptak@gmail.com>
2017-10-29 21:20:27 -0300

    ejercicio 6 terminado

--- Ruby/tp3/excepciones_ej6.rb
+++ Ruby/tp3/excepciones_ej6.rb
@@ -3,30 +3,37 @@
 # Como primer paso se pide al usuario que indique la cantidad de números que ingresará.
 
 cantidad = 0
-while cantidad < 15
-    puts '¿Cuál es la cantidad de números que ingresará? Debe ser al menos 15'
 
-    begin
-        cantidad = gets.to_i
-    rescue Exception
-        puts "Recuerde que solo puede ingresar números. Inténtelo nuevamente."
-        retry
-    end
-end
+puts '¿Cuál es la cantidad de números que ingresará? Debe ser al menos 15'
 
-# Luego se almacenan los números
+begin
+    cantidad = Integer(gets) 
+
+    raise Exception unless cantidad >= 15
+
+rescue ArgumentError
+    puts "Debe ingresar un número. Intente nuevamente:"
+    retry
+rescue Exception
+    puts "El número debe ser mayor a 15. Intente nuevamente:"
+    retry
+end    
+
 
 numeros = 1.upto(cantidad).map do
     puts 'Ingrese un número'
+
     begin
-    numero = gets.to_i
-    rescue Excepti
-        puts "Recuerde que solo puede ingresar números. Inténtelo nuevamente."
+        numero = Integer(gets)
+    
+    rescue ArgumentError
+        puts "Debe ingresar un número. Intente nuevamente:"
         retry
-    end
+    end 
+ end
+
+resultado = numeros.map do | x| 
+    x / (x - 1) 
+    raise ZeroDivisionError unless (x - 1) > 0 
 end
-
-# Y finalmente se imprime cada número dividido por su número entero inmediato anterior
-
-resultado = numeros.map { | x| x / (x - 1) rescue 'error'}
 puts 'El resultado es: %s' % resultado.join(', ')
