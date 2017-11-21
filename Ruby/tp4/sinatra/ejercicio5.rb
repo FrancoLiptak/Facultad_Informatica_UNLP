d8842b920a54d89be62a5eb6731e00f9075de817
Franco Liptak <francoliptak@gmail.com>
2017-11-21 19:06:16 -0300

    Ejercicio 5

--- Ruby/tp4/sinatra/ejercicio5.rb
+++ Ruby/tp4/sinatra/ejercicio5.rb
@@ -3,23 +3,41 @@ require 'sinatra/advanced_routes'
 Bundler.require
 
 get '/' do
+    s = "Las rutas disponibles son: <br>"
     Sinatra::Application.each_route do |route|
-        puts route.verb + " " + route.path
-      end
+        s << route.verb + " " + route.path + "<br>"
+    end
+    s
 end
 
 get '/mcm/:a/:b' do
-    puts (params['a'].to_i).lcm(params['b'].to_i)
+    a = params['a'].to_i
+    b= params['b'].to_i
+    a.lcm(b).to_s
 end
 
 get '/mcd/:a/:b' do
-    puts (params['a'].to_i).gcd(params['b'].to_i)
+    a = params['a'].to_i
+    b= params['b'].to_i
+    a.gcd(b).to_s
 end 
 
-get '/sum/*' do
-    puts params['splat'].to_a.inject(0){|sum,x| sum + x }
+get '/sum/*' do | path |
+    arrayOfIntegers = path.split('/').map(&:to_i)
+
+    ( arrayOfIntegers.inject(0){|sum,x| sum + x } ).to_s
 end
 
-get '/even/*' do
-    puts params['splat'].to_a
+get '/even/*' do | path |
+    arrayOfIntegers = path.split('/').map(&:to_i)
+
+    ( arrayOfIntegers.select(&:even?).size ).to_s
+end
+
+post '/random' do
+    rand.to_s
+end
+
+post '/random/:lower/:upper' do |lower, upper|
+    rand(lower..upper).to_s
 end
